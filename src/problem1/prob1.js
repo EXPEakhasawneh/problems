2ac65b82af32562e220d332ac705030c372240ff
EXPEakhasawneh <akhasawneh@expedia.com>
2019-06-02 02:55:38 +0300

    Add summation functions using for-loop & recursion

--- src/problem1/prob1.js
+++ src/problem1/prob1.js
@@ -6,3 +6,17 @@ export function sumArrForLoop(arr) {
   }
   return sum;
 }
+
+export function sumArrWhileLoop(arr) {
+  let sum = 0;
+  let i = arr.length;
+  while (i) {
+    sum += arr[i - 1];
+    i -= 1;
+  }
+  return sum;
+}
+
+export function sumArrRecur(arr) {
+  return arr.length === 0 ? 0 : arr[0] + sumArrRecur(arr.slice(1));
+}
--- src/problem1/prob1.test.js
+++ src/problem1/prob1.test.js
@@ -1,17 +1,80 @@
-import { sumArrForLoop } from './prob1';
+import { sumArrForLoop, sumArrWhileLoop, sumArrRecur } from './prob1';
 
-const positiveNumbers = [1, 2, 3, 4];
-const negativeNumbers = [-1, -2, -3, -4];
-const mixtureNumbers = [0, -1, 2, -3, 4];
+const testInputsOutputsObj = {
+  positiveNumbers: {
+    arr: [1, 2, 3, 4],
+    expected: 10,
+  },
+  negativeNumbers: {
+    arr: [-1, -2, -3, -4],
+    expected: -10,
+  },
+  mixtureNumbers: {
+    arr: [0, -1, 2, -3, 4],
+    expected: 2,
+  },
+  emptyArr: {
+    arr: [],
+    expected: 0,
+  },
+};
 
-test('sum of the numbers in positiveNumbers using a for-loop function', () => {
-  expect(sumArrForLoop(positiveNumbers)).toBe(10);
+test('sum of the numbers in positiveNumbers array using sumArrForLoop function', () => {
+  expect(sumArrForLoop(testInputsOutputsObj.positiveNumbers.arr))
+    .toBe(testInputsOutputsObj.positiveNumbers.expected);
 });
 
-test('sum of the numbers negativeNumbers using a for-loop function', () => {
-  expect(sumArrForLoop(negativeNumbers)).toBe(-10);
+test('sum of the numbers in negativeNumbers array using sumArrForLoop function', () => {
+  expect(sumArrForLoop(testInputsOutputsObj.negativeNumbers.arr))
+    .toBe(testInputsOutputsObj.negativeNumbers.expected);
 });
 
-test('sum of the numbers mixtureNumbers using a for-loop function', () => {
-  expect(sumArrForLoop(mixtureNumbers)).toBe(2);
+test('sum of the numbers in mixtureNumbers array using sumArrForLoop function', () => {
+  expect(sumArrForLoop(testInputsOutputsObj.mixtureNumbers.arr))
+    .toBe(testInputsOutputsObj.mixtureNumbers.expected);
+});
+
+test('sum of the numbers in emptyArr array using sumArrForLoop function', () => {
+  expect(sumArrForLoop(testInputsOutputsObj.emptyArr.arr))
+    .toBe(testInputsOutputsObj.emptyArr.expected);
+});
+
+test('sum of the numbers in positiveNumbers array using sumArrWhileLoop function', () => {
+  expect(sumArrWhileLoop(testInputsOutputsObj.positiveNumbers.arr))
+    .toBe(testInputsOutputsObj.positiveNumbers.expected);
+});
+
+test('sum of the numbers in negativeNumbers array using sumArrWhileLoop function', () => {
+  expect(sumArrWhileLoop(testInputsOutputsObj.negativeNumbers.arr))
+    .toBe(testInputsOutputsObj.negativeNumbers.expected);
+});
+
+test('sum of the numbers in mixtureNumbers array using sumArrWhileLoop function', () => {
+  expect(sumArrWhileLoop(testInputsOutputsObj.mixtureNumbers.arr))
+    .toBe(testInputsOutputsObj.mixtureNumbers.expected);
+});
+
+test('sum of the numbers in emptyArr array using sumArrWhileLoop function', () => {
+  expect(sumArrWhileLoop(testInputsOutputsObj.emptyArr.arr))
+    .toBe(testInputsOutputsObj.emptyArr.expected);
+});
+
+test('sum of the numbers in positiveNumbers array using sumArrRecur function', () => {
+  expect(sumArrRecur(testInputsOutputsObj.positiveNumbers.arr))
+    .toBe(testInputsOutputsObj.positiveNumbers.expected);
+});
+
+test('sum of the numbers in negativeNumbers array using sumArrRecur function', () => {
+  expect(sumArrRecur(testInputsOutputsObj.negativeNumbers.arr))
+    .toBe(testInputsOutputsObj.negativeNumbers.expected);
+});
+
+test('sum of the numbers in mixtureNumbers array using sumArrRecur function', () => {
+  expect(sumArrRecur(testInputsOutputsObj.mixtureNumbers.arr))
+    .toBe(testInputsOutputsObj.mixtureNumbers.expected);
+});
+
+test('sum of the numbers in emptyArr array using sumArrRecur function', () => {
+  expect(sumArrRecur(testInputsOutputsObj.emptyArr.arr))
+    .toBe(testInputsOutputsObj.emptyArr.expected);
 });
