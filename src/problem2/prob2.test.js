dbf4cb2236b91ecced48643517b2076b4f890bfc
EXPEakhasawneh <akhasawneh@expedia.com>
2019-06-03 23:04:30 +0300

    Add concat function(problem 2)

---
+++ src/problem2/prob2.test.js
@@ -0,0 +1,37 @@
+import concat from './prop2';
+
+test('concat two array', () => {
+  const toBeObject = {
+    arr1: ['a', 'b', 'c'],
+    arr2: [1, 2, 3],
+    expected: ['a', 1, 'b', 2, 'c', 3],
+  };
+  expect(concat(toBeObject.arr1, toBeObject.arr2)).toEqual(toBeObject.expected);
+});
+
+test('concat two array where length of the second array greater than first one', () => {
+  const toBeObject = {
+    arr1: ['a', 'b', 'c'],
+    arr2: [1, 2, 3, 4, 5],
+    expected: ['a', 1, 'b', 2, 'c', 3, 4, 5],
+  };
+  expect(concat(toBeObject.arr1, toBeObject.arr2)).toEqual(toBeObject.expected);
+});
+
+test('concat two array where length of the first array greater than first second', () => {
+  const toBeObject = {
+    arr1: ['a', 'b', 'c', 'd', 'e'],
+    arr2: [1, 2, 3],
+    expected: ['a', 1, 'b', 2, 'c', 3, 'd', 'e'],
+  };
+  expect(concat(toBeObject.arr1, toBeObject.arr2)).toEqual(toBeObject.expected);
+});
+
+test('concat two array where one of them is empty', () => {
+  const toBeObject = {
+    arr1: ['a', 'b', 'c'],
+    arr2: [],
+    expected: ['a', 'b', 'c'],
+  };
+  expect(concat(toBeObject.arr1, toBeObject.arr2)).toEqual(toBeObject.expected);
+});
---
+++ src/problem2/prop2.js
@@ -0,0 +1,14 @@
+export default function concat(list1, list2) {
+  const arr1 = Array.from(list1);
+  const arr2 = Array.from(list2);
+  const result = [];
+  while (arr1.length || arr2.length) {
+    if (arr1.length) {
+      result.push(arr1.shift());
+    }
+    if (arr2.length) {
+      result.push(arr2.shift());
+    }
+  }
+  return result;
+}
